3e5521f60ed185389dd85ba5ca5a87c505cd4da3
Titir Singh <titirpurulia@gmail.com>
2025-06-29 13:44:07 +0530

    breadcrumb separated

--- resources/views/Frontend/teacher-dashboard/navbar.blade.php
+++ resources/views/Frontend/teacher-dashboard/navbar.blade.php
@@ -1,25 +1,25 @@
 <ul class="dashboard-tabs nav nav-tabs nav-justified mb-4" role="tablist">
     <li class="nav-item">
         <a href="{{ route('teacher.profile.index') }}"
-           class="nav-link {{ request()->routeIs('teacher.profile.index') ? 'active' : '' }}">
+           class="nav-link {{ request()->routeIs('teacher.dashboard') || request()->is('teacher/profile*') ? 'active' : '' }}">
            Profile
         </a>
     </li>
     <li class="nav-item">
         <a href="{{ route('teacher.courses.index') }}"
-           class="nav-link {{ request()->routeIs('teacher.courses.index') ? 'active' : '' }}">
+           class="nav-link {{ request()->is('teacher/courses*') ? 'active' : '' }}">
            Manage Courses
         </a>
     </li>
     <li class="nav-item">
         <a href="{{ route('teacher.remarks.index') }}"
-           class="nav-link {{ request()->routeIs('teacher.remarks.index') ? 'active' : '' }}">
+           class="nav-link {{ request()->is('teacher/remarks*') ? 'active' : '' }}">
            Remarks
         </a>
     </li>
     <li class="nav-item">
         <a href="{{ route('teacher.announcements.index') }}"
-           class="nav-link {{ request()->routeIs('teacher.announcements.index') ? 'active' : '' }}">
+           class="nav-link {{ request()->is('teacher/announcements*') ? 'active' : '' }}">
            Announcements
         </a>
     </li>
